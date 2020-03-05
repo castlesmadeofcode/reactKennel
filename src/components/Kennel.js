3dc29e9726c789d4bde80c7ad06b328b8b1801ef
castle <castlesmadeofcode@gmail.com>
2020-03-04 22:15:41 -0600

    Chapter 15 - React: Logout on Navbar Functionality

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import NavBar from "./nav/NavBar";
 import ApplicationViews from "./ApplicationViews";
+
 import "./Kennel.css";
 
 const Kennel = () => {
   // Check if credentials are in session storage returns true/false
   const isAuthenticated = () => sessionStorage.getItem("credentials") !== null;
 
+  // Will be passing this state down to reroute users who are not "logged in"
   const [hasUser, setHasUser] = useState(isAuthenticated());
 
   const setUser = user => {
@@ -14,9 +16,14 @@ const Kennel = () => {
     setHasUser(isAuthenticated());
   };
 
+  const clearUser = () => {
+    sessionStorage.clear();
+    setHasUser(isAuthenticated());
+  };
+
   return (
     <>
-      <NavBar hasUser={hasUser} />
+      <NavBar hasUser={hasUser} clearUser={clearUser} />
       <ApplicationViews hasUser={hasUser} setUser={setUser} />
     </>
   );
